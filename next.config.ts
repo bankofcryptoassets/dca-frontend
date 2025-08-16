d5840878df6995665a78737dc4c8ae1b38361200
Krushang Kasundra <kruzkasu223@gmail.com>
2025-08-16 14:50:59 +0530

    chore: update farcaster redirects

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,16 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/.well-known/farcaster.json',
-  //       destination:
-  //         'https://api.farcaster.xyz/miniapps/hosted-manifest/<app-id>',
-  //       permanent: false,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination:
+          'https://api.farcaster.xyz/miniapps/hosted-manifest/0198b22d-626d-b7dc-21d1-05b875a58dce',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
