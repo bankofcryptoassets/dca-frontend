a457a946c5c7d2ae67cd99971c2738542a004bf3
Krushang Kasundra <kruzkasu223@gmail.com>
2025-08-16 14:42:13 +0530

    chore: added farcaster manifest file

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,16 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination:
-          'https://api.farcaster.xyz/miniapps/hosted-manifest/<app-id>',
-        permanent: false,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/.well-known/farcaster.json',
+  //       destination:
+  //         'https://api.farcaster.xyz/miniapps/hosted-manifest/<app-id>',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
 }
 
 export default nextConfig
